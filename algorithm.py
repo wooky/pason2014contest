87b78014d7a0849b1d0e958edae5a6624213e87d
teamasdf <contestant@ip-10-28-101-29.ec2.internal>
2014-11-15 18:08:14 +0000

    stuff

--- algorithm.py
+++ algorithm.py
@@ -25,19 +25,32 @@ class Algorithm():
 				# Get closest enemy
 				dist_enemy = (enemy['position'][0]-tank['position'][0])**2+(enemy['position'][1]-tank['position'][1])**2
 				closest_enemy = (closest_opponent_pos[0]-tank['position'][0])**2+(closest_opponent_pos[1]-tank['position'][1])**2
-				if(dist_enemy < closest_enemy): closest_opponent_pos = enemy['position']
+				if(dist_enemy < closest_enemy):
+					closest_opponent_pos = enemy['position']
+					closest_tank_id = enemy['id']
 				# Check if we should shoot
 				angle = self.atan2positive(enemy['position'][1]-tank['position'][1], enemy['position'][0]-tank['position'][0])-tank['turret']
 				# Only shoot if we are within 9 degrees of an opponent
 				if math.fabs(angle) < math.pi/20.0:
 					should_shoot = True
-			if closest_opponent_pos[0] != float("inf"):
+			if closest_opponent_pos[0] != float("inf") and hasattr(self, 'bulletSpeed'):
+				if hasattr(self, 'last_opponent'):
+					prevpos = [x for x in self.last_opponent['tanks'] if x['id']==closest_tank_id]
+					if prevpos:
+						prevpos = prevpos[0]['position']
+						dt = 0.001 * (a['timestamp'] - self.last_a['timestamp'])
+						v = [(closest_opponent_pos[0] - prevpos[0]) / dt,(closest_opponent_pos[1] - prevpos[1]) / dt]
+						bt = math.sqrt(math.pow(closest_opponent_pos[0]-tank['position'][0], 2)+ math.pow(closest_opponent_pos[1]-tank['position'][1], 2))/self.bulletSpeed
+						interpolate = [closest_opponent_pos[0]+bt*v[0], closest_opponent_pos[1]+bt*v[1]]
+						#print closest_opponent_pos, interpolate
+						closest_opponent_pos = interpolate
 				calc = self.atan2positive(closest_opponent_pos[1]-tank['position'][1], closest_opponent_pos[0]-tank['position'][0])
 				diff = calc-tank['turret']
 				self.send({'comm_type':'ROTATE_TURRET','client_token':token,'direction':'CCW' if diff>0 else 'CW','rads':math.fabs(diff),'tank_id':tank_id})
 				if should_shoot:
 					self.send({'comm_type':'FIRE','client_token':token,'tank_id':tank_id})
 		self.last_opponent = opponent
+		self.last_a = a
 				
 	def __init__(self,client):
 		self.com = client.comm
